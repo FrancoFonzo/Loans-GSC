8f942114dfefcb2390f860a42c4eed94af1ff5d4
Franco Fonzo <francofonzo1@gmail.com>
2022-11-21 00:24:48 -0300

    Add Loan IsReturned (Status) calculated property

--- MVC/Entities/Loan.cs
+++ MVC/Entities/Loan.cs
@@ -1,13 +1,18 @@
-﻿namespace MVC.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVC.Entities
 {
     public class Loan : EntityBase
     {
         public DateTime CreateDate { get; set; }
         public DateTime? ReturnDate { get; set; }
-
+        
         public int PersonId { get; set; }
         public Person? Person { get; set; }
         public int ThingId { get; set; }
         public Thing? Thing { get; set; }
+
+        [NotMapped]
+        public bool IsReturned => ReturnDate.HasValue;
     }
 }
